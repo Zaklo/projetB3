daaf5d28b1065ba8d3ed23c6458b90179ff1b4d3
Antoine Lozach <lozachantoine@gmail.com>
2020-03-28 17:28:58 +0100

    adding-popup-options

--- src/script.js
+++ src/script.js
@@ -13,17 +13,19 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 //get the datas from API "que faire à Paris"
 async function getData() {
     let url =
-        'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type';
+        'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1)&rows=50&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type';
     let response = await fetch(url);
 
     let data = await response.json();
 
-    data.records.forEach(function(event) {
+    data.records.forEach(function (event) {
         // le titre de l'événement
         let title = event.fields.title;
 
+        var cover_url = event.fields.cover_url;
+
         // on vérifie que l'événement a bien le champs 'lat_lon'
-        if(event.fields.lat_lon) {
+        if (event.fields.lat_lon) {
 
             // si oui, on ajoute le marqueur
 
@@ -31,15 +33,20 @@ async function getData() {
             let latitude = event.fields.lat_lon[0];
             // la longitude
             let longitude = event.fields.lat_lon[1];
-            //le nom de l'event
-            let title = event.fields.title;
 
+            var marker = L.marker([latitude, longitude]);
 
+            var popup = L.popup(maxWidth = 40)
+                .setLatLng([latitude, longitude])
+                .setContent('<p>' + title + '</p><br/><img style="max-width: 100px" src="' + cover_url + '">');
 
-            var marker = L.marker([latitude, longitude]).addTo(mymap);
-            marker.bindPopup(title).openPopup();
+            var customOptions = {'maxWidth': '100', 'maxHeight': '200'};
+            marker.bindPopup(popup, customOptions).openPopup();
+
+            marker.addTo(mymap);
         }
 
     });
 }
+
 getData()
