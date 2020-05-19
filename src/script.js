f35117b3fdd9d4c24ba94b762e960bc599cce4b4
Sarah <sarahprs@icloud.com>
2020-05-19 17:54:19 +0200

    changement-couleur-carte

--- src/script.js
+++ src/script.js
@@ -2,10 +2,11 @@
 var mymap = L.map('mapid').setView([48.856613, 2.352222], 13);
 var layerGroup = L.layerGroup().addTo(mymap);
 
+// création de la map avec ses attributs
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiemFrbG8iLCJhIjoiY2s4YnBxcHE0MGU4ZTNmbWo2ZnlxNGNoayJ9.PfLD5GhOPBE4WjAn8PlZug'
