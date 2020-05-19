7474029ead7752b60f0c38631b40f44f9663e71b
Sarah <sarahprs@icloud.com>
2020-05-19 17:04:19 +0200

    adding-search-form

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 //set the map
 var mymap = L.map('mapid').setView([48.856613, 2.352222], 13);
+var layerGroup = L.layerGroup().addTo(map);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,9 +12,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 //get the datas from API "que faire à Paris"
-async function getData() {
+async function getData(query) {
+    if(query == undefined){
+        query = " ";
+    }
     let url =
-        'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1)&rows=50&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type';
+        'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=' + query + '&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1)&rows=50&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type';
     let response = await fetch(url);
 
     let data = await response.json();
@@ -43,7 +47,7 @@ async function getData() {
                 .setContent('<p class="popup-title">' + title + '</p><br/><img class="popup-image" src="'
                     + cover_url + '">');
 
-            var customOptions = {'maxWidth': '200', 'maxHeight': '200'};
+            var customOptions = { 'maxWidth': '200', 'maxHeight': '200' };
             marker.bindPopup(popup, customOptions).openPopup();
 
             marker.addTo(mymap);
@@ -53,3 +57,9 @@ async function getData() {
 }
 
 getData()
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    getData(search.value);
+
+}
